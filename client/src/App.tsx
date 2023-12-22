8452bce1dcf51a1f4a4c1405b2f818d6dcd8c066
Akash Singh <developer.akash8@gmail.com>
2023-12-23 01:02:34 +0530

    app: create header & footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,21 @@
+import style from "./assets/styles/app.module.css";
+
 function App() {
   return (
-    <main>
-      <h1>SANTaCHAT</h1>
-    </main>
+    <div className={style.app}>
+      <header className={style.header}>
+        <h1>SANTaCHAT</h1>
+        <p>CONNECTING STUDENTS, CREATING COMMUNITY!</p>
+      </header>
+
+      <footer className={style.footer}>
+        <ul>
+          <li>COPYRIGHT</li>
+          <li>2023-2024</li>
+          <li>SANTaCHAT</li>
+        </ul>
+      </footer>
+    </div>
   );
 }
 
