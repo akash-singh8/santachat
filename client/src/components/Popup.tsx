e49da180292914261c881a74ff3552b59a3fb831
Akash Singh <developer.akash8@gmail.com>
2023-12-27 23:09:30 +0530

    popup: handle verify request

--- client/src/components/Popup.tsx
+++ client/src/components/Popup.tsx
@@ -6,6 +6,31 @@ import popState from "../store/popup";
 const Popup = () => {
   const popup = useRecoilValue(popState);
 
+  const verifyUser = async (e: any) => {
+    e.preventDefault();
+
+    const email = document.querySelector(
+      `.${style.input} input`
+    ) as HTMLInputElement;
+
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/auth/sendlink?user=${email.value}`,
+        {
+          method: "POST",
+        }
+      );
+
+      const data = await res.json();
+      alert(data.message);
+
+      email.value = "";
+    } catch (e) {
+      console.log(e);
+      alert("Unable to verify at present moment!");
+    }
+  };
+
   return (
     popup && (
       <div className={style.container}>
@@ -19,10 +44,12 @@ const Popup = () => {
             </div>
 
             <div className={style.verify}>
-              <div className={style.input}>
-                <input type="text" placeholder="student@scu.edu" />
-                <button className={style.button}>verify</button>
-              </div>
+              <form className={style.input} onSubmit={verifyUser}>
+                <input type="email" placeholder="student@scu.edu" required />
+                <button className={style.button} type="submit">
+                  verify
+                </button>
+              </form>
               <p>Please verify your SCU email address</p>
             </div>
           </div>
