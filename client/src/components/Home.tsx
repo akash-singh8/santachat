b2a7321bd18890dc98ac2036f2ec405248ba1dd8
Akash Singh <developer.akash8@gmail.com>
2023-12-28 23:56:17 +0530

    home: add dynamic navigation

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,9 +2,20 @@ import style from "../assets/styles/home.module.css";
 import demo_chat from "../assets/images/chat.png";
 import { useSetRecoilState } from "recoil";
 import popState from "../store/popup";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const setPopup = useSetRecoilState(popState);
+  const navigate = useNavigate();
+  const logged = localStorage.getItem("authToken");
+
+  const handleClick = () => {
+    if (logged) {
+      navigate("/chat");
+    } else {
+      setPopup(true);
+    }
+  };
 
   return (
     <>
@@ -14,8 +25,8 @@ const Home = () => {
           ANONYMOUSLY
         </p>
 
-        <button className="button" onClick={() => setPopup(true)}>
-          Get Started &#x27F6;
+        <button className="button" onClick={handleClick}>
+          {logged ? "Chat Anonymously" : "Get Started"} &#x27F6;
         </button>
 
         <img src={demo_chat} alt="Demo Chat" />
