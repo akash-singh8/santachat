f53afe0dc0dd708d30fc8f10af4ef1d77d90b31d
Akash Singh <developer.akash8@gmail.com>
2023-12-29 16:01:35 +0530

    ws: connect client w/ similar interests

--- server/src/controller/initiate.ts
+++ server/src/controller/initiate.ts
@@ -1,12 +1,24 @@
 import WebSocket from "ws";
 import jwt from "jsonwebtoken";
 import { disconnectClient } from "./close";
-import { client, singles } from "./ws";
+import {
+  client,
+  waitingClients_NoInterest,
+  waitingClients_Interest,
+} from "./ws";
 
-const initiateConnection = (auth: string, ws: WebSocket) => {
-  let user: string = "";
+const initiateConnection = (
+  auth: string,
+  ws: WebSocket,
+  userInterest?: string[]
+) => {
+  let user: string;
   try {
-    user = jwt.verify(auth!, process.env.JWT_SECRET!) as string;
+    const payload = jwt.verify(
+      auth!,
+      process.env.JWT_SECRET!
+    ) as jwt.JwtPayload;
+    user = payload.email;
   } catch (e) {
     return disconnectClient(ws);
   }
@@ -22,35 +34,86 @@ const initiateConnection = (auth: string, ws: WebSocket) => {
   }
 
   client[user] = {
+    interests: userInterest,
     ws: ws,
   };
 
-  // connecting two random users together
-  const partner = singles.pop();
-  if (partner) {
-    client[user].partner = partner;
-    client[partner].partner = user;
+  if (userInterest) {
+    // Connecting two user with similar interest
+    let partnerFound = false;
 
-    client[user].ws.send(
-      JSON.stringify({
-        status: 202,
-        message: `Partner found: ${partner}`,
-      })
-    );
-    client[partner].ws.send(
-      JSON.stringify({
-        status: 202,
-        message: `Partner found: ${user}`,
-      })
-    );
+    userInterest.forEach((interest) => {
+      const availableClients = Object.keys(waitingClients_Interest[interest]);
+
+      if (availableClients.length > 0) {
+        const partner = availableClients[0];
+        client[user].partner = partner;
+        client[partner].partner = user;
+
+        // remove the partner from all the waitingClients object
+        const partnerInterest = client[partner].interests;
+        partnerInterest?.forEach((interest) => {
+          delete waitingClients_Interest[interest][partner];
+        });
+
+        client[user].ws.send(
+          JSON.stringify({
+            status: 202,
+            message: `Partner found: ${partner}`,
+          })
+        );
+        client[partner].ws.send(
+          JSON.stringify({
+            status: 202,
+            message: `Partner found: ${user}`,
+          })
+        );
+
+        partnerFound = true;
+        return;
+      }
+    });
+
+    // store current user with their interests, in waitingClients object
+    if (!partnerFound) {
+      userInterest?.forEach((interest) => {
+        waitingClients_Interest[interest][user] = true;
+      });
+      ws.send(
+        JSON.stringify({
+          status: 201,
+          message: "Waiting for a partner!",
+        })
+      );
+    }
   } else {
-    singles.push(user);
-    ws.send(
-      JSON.stringify({
-        status: 201,
-        message: "Waiting for a partner!",
-      })
-    );
+    // connecting two users with no interests
+    const partner = waitingClients_NoInterest.pop();
+    if (partner) {
+      client[user].partner = partner;
+      client[partner].partner = user;
+
+      client[user].ws.send(
+        JSON.stringify({
+          status: 202,
+          message: `Partner found: ${partner}`,
+        })
+      );
+      client[partner].ws.send(
+        JSON.stringify({
+          status: 202,
+          message: `Partner found: ${user}`,
+        })
+      );
+    } else {
+      waitingClients_NoInterest.push(user);
+      ws.send(
+        JSON.stringify({
+          status: 201,
+          message: "Waiting for a partner!",
+        })
+      );
+    }
   }
 
   console.log("Client connected: ", user);
--- server/src/controller/ws.ts
+++ server/src/controller/ws.ts
@@ -7,20 +7,43 @@ import { prisma } from "../index";
 type wsClients = {
   [name: string]: {
     partner?: string;
+    interests?: string[];
     ws: WebSocket;
   };
 };
 
+type WaitingClient_Interst = {
+  [club: string]: {
+    [client: string]: boolean;
+  };
+};
+
+const interests = [
+  "Clubs",
+  "Rooms",
+  "Study",
+  "Chill",
+  "Freshman",
+  "Sophomore",
+  "Junior",
+  "Senior",
+];
 let totalClients = 0;
 export const client: wsClients = {};
-export const singles: string[] = [];
+export const waitingClients_NoInterest: string[] = [];
+export const waitingClients_Interest: WaitingClient_Interst = {};
+
+interests.forEach((i) => {
+  waitingClients_Interest[i] = {};
+});
 
 const wsConnectionHandler = (ws: WebSocket, req: http.IncomingMessage) => {
   const url = new URL(req.url!, `${process.env.SERVER_BASE_URL}`);
   const auth = url.searchParams.get("auth");
   if (!auth) return disconnectClient(ws);
 
-  const user = initiateConnection(auth, ws);
+  const userInterest = url.searchParams.get("interests")?.split("_");
+  const user = initiateConnection(auth, ws, userInterest);
   if (!user) return;
 
   totalClients += 1;
@@ -62,7 +85,17 @@ const wsConnectionHandler = (ws: WebSocket, req: http.IncomingMessage) => {
 
   ws.on("close", () => {
     totalClients -= 1;
-    if (!client[user]?.partner) singles.pop();
+    // we don't need to remove both of the clients, instead we'll remove the current client and add the other client in waiting list
+    if (!client[user]?.partner) {
+      const userInterest = client[user].interests;
+      if (userInterest) {
+        userInterest.forEach((interest) => {
+          delete waitingClients_Interest[interest][user];
+        });
+      } else {
+        waitingClients_NoInterest.pop();
+      }
+    }
     wsCloseHandler(user);
   });
 };
