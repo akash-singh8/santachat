a42fef7d1bd4d4913069034315df44f40edc943e
Akash Singh <developer.akash8@gmail.com>
2024-01-07 18:02:01 +0530

    fix: feedback schema

--- server/src/validation/feedback.ts
+++ server/src/validation/feedback.ts
@@ -2,13 +2,13 @@ import { z } from "zod";
 
 const feedbackSchema = z.object({
   user: z.string().email(),
-  to: z.string().email(),
+  to: z.string(),
   feedback: z.string(),
   rating: z
     .number()
     .min(0, "Rating must be greater than or equal to 0")
     .max(5, "Rating must be less than or equal to 5"),
-  createdAt: z.date(),
+  createdAt: z.date().nullable(),
 });
 
 export default feedbackSchema;
