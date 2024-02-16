efbf09b29b7e1829938d76e757b7f05890046e0a
Akash Singh <developer.akash8@gmail.com>
2024-02-16 22:08:21 +0530

    header: update styles

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,9 +3,19 @@ import style from "../assets/styles/header.module.css";
 import user from "../assets/images/user.svg";
 import auth from "../store/auth";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 const Header = () => {
   const logged = useRecoilValue(auth);
+
+  useEffect(() => {
+    if (window.innerWidth < 601 && !logged) {
+      const header = document.querySelector(
+        `.${style.header}`
+      ) as HTMLDivElement;
+      header.style.alignItems = "center";
+    }
+  }, [logged]);
 
   return (
     <header className={style.header}>
