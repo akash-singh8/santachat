a070e15c59d07e23dad8663295071b91f8e71819
Akash Singh <developer.akash8@gmail.com>
2024-03-25 16:41:02 +0530

    update user page

--- client/src/components/User.tsx
+++ client/src/components/User.tsx
@@ -1,13 +1,31 @@
-import style from "../assets/styles/user.module.css";
-import user from "../assets/images/user.svg";
-import { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import auth from "../store/auth";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import userState from "../store/user";
 import { toast } from "react-toastify";
 
+import Button from "../shared/Button";
+
+import style from "../assets/styles/user.module.css";
+import user from "../assets/images/user.svg";
+
+import auth from "../store/auth";
+import userState from "../store/user";
+import {
+  Box,
+  Typography,
+  useTheme,
+  TableContainer,
+  Table,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableCell,
+  styled,
+} from "@mui/material";
+
 const User = () => {
+  const theme = useTheme();
+
   const [authToken, setAuthToken] = useRecoilState(auth);
   const userData = useRecoilValue(userState);
   const navigate = useNavigate();
@@ -26,50 +44,83 @@ const User = () => {
     navigate("/");
   };
 
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.primary[200],
+    },
+    td: {
+      color: theme.palette.primary[800],
+      borderBottom: `1px solid ${theme.palette.primary[500]}`,
+    },
+    th: {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.primary[100],
+      fontWeight: 600,
+      border: 0,
+    },
+  }));
+
   return (
     <div className={style.user}>
       <div className={style.detail}>
-        <img src={user} alt="user profile" />
-        <h2>{userData.user}</h2>
+        <Box
+          className={style.userLogo}
+          sx={{ border: `2px solid ${theme.palette.primary.main}` }}
+        >
+          <img src={user} alt="user profile" />
+        </Box>
+        <Typography
+          variant="h2"
+          sx={{ fontFamily: "Poppins", color: theme.palette.primary[800] }}
+        >
+          {userData.user}
+        </Typography>
 
-        <button className="button" onClick={handleLogout}>
-          logout
-        </button>
+        <Button onClick={handleLogout}>Logout</Button>
       </div>
 
       <div className={style.feedback}>
-        <h2>Feedback history</h2>
+        <Typography
+          variant="h2"
+          sx={{ fontFamily: "Poppins", color: theme.palette.primary[800] }}
+        >
+          Feedback history
+        </Typography>
 
-        <table className={style.table}>
-          <thead>
-            <tr>
-              <th>Rating</th>
-              <th>Feedback</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {userData.feedbacks?.length ? (
-              userData.feedbacks.map((feedback) => {
-                const time = new Date(feedback.createdAt);
+        <TableContainer sx={{ borderRadius: "8px" }}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <StyledTableRow>
+                <TableCell>Feedback</TableCell>
+                <TableCell align="right">Rating</TableCell>
+                <TableCell align="right">Date</TableCell>
+              </StyledTableRow>
+            </TableHead>
+            <TableBody>
+              {userData.feedbacks?.length ? (
+                userData.feedbacks.map((feedback) => {
+                  const time = new Date(feedback.createdAt);
 
-                return (
-                  <tr className={style.row} key={feedback.id}>
-                    <td>{feedback.rating} / 5</td>
-                    <td>{feedback.feedback}</td>
-                    <td title={time.toTimeString()}>
-                      {time.toLocaleDateString()}
-                    </td>
-                  </tr>
-                );
-              })
-            ) : (
-              <tr className={style.nofeedback}>
-                <td>No feedbacks to show</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
+                  return (
+                    <StyledTableRow key={feedback.id}>
+                      <TableCell scope="row">{feedback.feedback}</TableCell>
+                      <TableCell align="right">{feedback.rating}</TableCell>
+                      <TableCell align="right" title={time.toTimeString()}>
+                        {time.toLocaleDateString()}
+                      </TableCell>
+                    </StyledTableRow>
+                  );
+                })
+              ) : (
+                <StyledTableRow>
+                  <TableCell>No feedbacks to show</TableCell>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
+                </StyledTableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   );
